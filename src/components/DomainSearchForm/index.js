0f73f0221a3ff2a8fa3cd7538ec57ee3b9daa72d
wzeddie <89380062@qq.com>
2024-06-11 15:32:09 +0800

    1

--- src/components/DomainSearchForm/index.js
+++ src/components/DomainSearchForm/index.js
@@ -35,7 +35,7 @@ const DomainSearchForm = ({ initialDomains }) => {
   };
 
   return (
-    <section className="bg-gray-50">
+    <section className="container bg-gray-50">
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // // 定位index.html文件
+  //单独编译，命令 npx tailwindcss -i ./src/input.css -o ./dist/output.css
+
   content: [
     './src/**/*.{js,jsx,ts,tsx}', // Adjust this path according to your project structure
     './public/index.html',
